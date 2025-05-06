eac1f588f16f53315e80461d6384af661f40f84b
Fabrizio <u201914181@upc.edu.pe>
2025-05-06 14:45:09 -0500

    feat(language-switcher): add language switcher component

---
+++ src/public/components/language-switcher.component.vue
@@ -0,0 +1,16 @@
+<script>
+
+
+export default {
+  name: "language-switcher",
+
+}
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale" :options="$i18n.availableLocales()" class="uppercase"/>
+</template>
+
+<style scoped>
+
+</style>
