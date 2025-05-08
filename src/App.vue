fdebb6382cad3b675f79e786a26daaa3dd984ecd
Fabrizio <u201914181@upc.edu.pe>
2025-05-08 14:28:17 -0500

    feat(main): show main components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script>
 
+import {defineComponent} from "vue";
+import LanguageSwitcher from "./public/components/language-switcher.component.vue";
+import CreateQuote from "./quote-management/components/create-quote.component.vue";
+import QuoteOrderList from "./quote-management/components/quote-order-list.component.vue"
+import HeaderContent from "./public/components/header-content.component.vue";
+import ToolbarContent from "./public/components/toolbar-content.component.vue";
+export default defineComponent({
+  components: {ToolbarContent, HeaderContent, CreateQuote, LanguageSwitcher,QuoteOrderList}
+})
 </script>
 <template>
+  <header-content></header-content>
+  <div class="flex">
+    <toolbar-content></toolbar-content>
+    <quote-order-list class="w-full"></quote-order-list>
+  </div>
 
 </template>
 <style scoped>
