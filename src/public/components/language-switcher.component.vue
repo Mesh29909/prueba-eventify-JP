c3f3d92a89aef57091b9288044ca729698b37528
Fabrizio <u201914181@upc.edu.pe>
2025-05-08 14:19:12 -0500

    fix(language-switcher): fix options attribute of language switcher

--- src/public/components/language-switcher.component.vue
+++ src/public/components/language-switcher.component.vue
@@ -8,7 +8,7 @@ export default {
 </script>
 
 <template>
-  <pv-select-button v-model="$i18n.locale" :options="$i18n.availableLocales()" class="uppercase"/>
+  <pv-select-button style="background-color:#3A506B" v-model="$i18n.locale" :options="$i18n.availableLocales" class="uppercase"/>
 </template>
 
 <style scoped>
