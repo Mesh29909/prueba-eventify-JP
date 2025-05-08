28430a05fea73393e5696e1cc91ff325074ef28d
Fabrizio <u201914181@upc.edu.pe>
2025-05-08 14:23:50 -0500

    feat(header): add header component

---
+++ src/public/components/header-content.component.vue
@@ -0,0 +1,31 @@
+<script>
+import LanguageSwitcher from "./language-switcher.component.vue";
+
+export default {
+  name: "header-content",
+  components: {LanguageSwitcher}
+}
+</script>
+
+<template>
+  <pv-toolbar style="background-color: #3A506B; border-radius:0;">
+    <template #start>
+      <div class="flex align-items-center justify-content-center">
+        <img src="../../assets/eventify-logo.png" alt="eventify-logo">
+      </div>
+
+    </template>
+    <template #end>
+      <div class="flex align-items-center justify-content-center">
+        <language-switcher></language-switcher>
+        <pv-avatar class="ml-4" label="U" shape="circle"></pv-avatar>
+      </div>
+    </template>
+  </pv-toolbar>
+</template>
+
+<style scoped>
+  img{
+    width: 150px;
+  }
+</style>
