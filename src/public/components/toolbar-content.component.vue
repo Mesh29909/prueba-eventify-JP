32de1e40049679f50378c3c7001369f428e92604
Fabrizio <u201914181@upc.edu.pe>
2025-05-08 14:24:09 -0500

    feat(toolbar): add toolbar component

---
+++ src/public/components/toolbar-content.component.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "toolbar-content",
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-toolbar style="border-radius:0; background-color:#fff; border:none; padding:0">
+    <template #start>
+      <div class="h-4rem"></div>
+    </template>
+    <template #center>
+      <div class="flex flex-column">
+        <a href="#">{{ $t('toolbar.profile') }}</a>
+        <a href="#">{{ $t('toolbar.calendar') }}</a>
+        <a href="#">{{ $t('toolbar.events') }}</a>
+        <a href="#">{{ $t('toolbar.tasks') }}</a>
+        <a href="#">{{ $t('toolbar.quotes') }}</a>
+        <a href="#">{{ $t('toolbar.messages') }}</a>
+        <a href="#">{{ $t('toolbar.subscriptions') }}</a>
+      </div>
+    </template>
+    <template #end>
+      <div class="h-4rem"></div>
+    </template>
+  </pv-toolbar>
+</template>
+
+<style scoped>
+  a{
+
+    padding: 16px 48px;
+    text-decoration: none;
+    color: #000;
+  }
+  a:hover{
+    background-color: #3A506B;
+    color: #fff;
+    transition: 0.3s ease-in-out;
+  }
+</style>
